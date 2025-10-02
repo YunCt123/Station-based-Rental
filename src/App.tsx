5b6e7eefd74815fbf6d00dddf60b94ab5653918a
YunCt123 <yunct1234@gmail.com>
2025-10-02 21:10:59 +0700

    add stations - howitworks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Stations from './pages/Stations';
 import HowItWorks from './pages/HowItWorks';
@@ -10,11 +9,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login onLogin={() => { }} />} />
         <Route path="/stations" element={<Stations />} />
         <Route path="/how-it-works" element={<HowItWorks />} />
-        {/* Add more routes here as needed */}
-        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
   );

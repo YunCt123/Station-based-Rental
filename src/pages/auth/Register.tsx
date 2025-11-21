8fb4a3914bad0bc429f2ee258476e5e63bc95feb
Trong Tri Bui <buitrongtri2004@gmail.com>
2025-11-21 16:02:43 +0700

    Update Register.tsx

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -316,7 +316,7 @@ const Register = ({ onRegister: _onRegister }: RegisterProps) => {
                   }
                 />
                 <label htmlFor="terms" className="text-sm leading-relaxed">
-                  {t("register.agreeToTermsText")}{" "}
+                 Tôi {" "}
                   <Link
                     to="/policy"
                     className="text-primary hover:text-primary-dark"
